35bfdc081517faa55ba22af37040d8cf03297927
Jessica-Oliveros-P <jessoli567@gmail.com>
2022-09-13 14:18:51 -0500

    ejercicio 5

---
+++ 5 Números en JavaScript/Números en JavaScript.js
@@ -0,0 +1,17 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let alturacm = 165;
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let alturamts = 1.65;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesokg = 62.5;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaredondeandoarriba = Math.ceil(alturamts);
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoredondeandoabajo = Math.floor(pesokg);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let maxvaluejs = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
