ab8bd8a2f8aa649a093798c6210262b4e9e26b75
Jessica-Oliveros-P <jessoli567@gmail.com>
2022-09-13 09:48:51 -0500

    Ejercicio 2

---
+++ 2.Sintaxis, variables y palabras reservadas de JS/Sintaxis, variables y palabras reservadas de JS.js
@@ -0,0 +1,12 @@
+const ejercicio =[ 
+    "Jessica Oliveros",
+    30,
+    true,
+    new Date(1991, 8 ,28),
+    {
+        titulo: 'El juego de Ender',
+        autor: 'Orson Scott Card', 
+        fecha: new Date(2018, 6 ,3),
+        url: 'https://www.amazon.com/-/es/Orson-Scott-Card/dp/8420434191/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1663038749&sr=1-1',
+    },
+]
